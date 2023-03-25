d8bcd4388cf4bb78bd474555de1295bb14648a5e
Navneet-Patnecha <navneetchippa@gmail.com>
2023-03-25 12:08:59 +0530

    made this game more user friendly

--- Guess Game.py
+++ Guess Game.py
@@ -1,35 +1,62 @@
-Player_name = input("Enter Your Name:  ")
-print(f"Welcome!!{Player_name} to guessing word game")
-word = "internship"
-guesses = " "
+import random
+
+name = input("Enter your name: ")
+print(f"Hello {name}! Welcome to the guessing game!")
+words = ["guessing","apple","earphones","macbook","python",
+        "television", "sunset","opensource"]
+# get a random word from this list
+index = random.randint(0, len(words))
+word = words[index]
+indexes = random.sample(range(0, len(word)), 3)
+guesses = "" #characters that the user has guessed so far
+for i in indexes:
+    guesses += word[i]
 chances = 10
+play = "Yes"
 
-while chances >0:
-    for ch in word:
+def playagain():
+    # this will help you understad scoping
+    global play
+    play = input("Do you want to play again? (Yes/No): ")
+    if play == "Yes":
+        global chances, word, guesses
+        chances = 10
+        index = random.randint(0, len(words))
+        word = words[index]
+        indexes = random.sample(range(0, len(word)), 3)
+        guesses = "" 
+        for i in indexes:
+            guesses += word[i]
+
+# this play is global variable
+while play == "Yes":
+    while chances > 0:
         won = True
-        if ch in guesses:
-            print(ch,end=" ")
-        else:
-            print("_",end=" ")
-            won = False
+        for ch in word:
+            if ch in guesses: # the person has guessed
+                print(ch, end=" ")
+            else:
+                print("_", end=" ")
+                won = False
 
-
-    if won:
-        print("You Won!!!")
-        print(f"{Player_name} , your score is {chances*10}")
-        break
-
-
-
-    #take input from user
-    guess = input("\n guess a character!!")
-    guesses +=guess
-
-    if guess not in word:
-        chances -=1
-        print("you guessed wrong character")
-        print(f"you are left with {chances} chances")
-
-        if chances == 0:
-            print("Haar gaya bhai m toh")
+        if won:
+            print("\nYou Won!")
+            print(f"Your score is {chances * 10}")
+            playagain()
             break
+
+        # take a guess from the user
+        guess = input("\nGuess a character: ")
+        guesses += guess
+
+        if guess not in word:
+            chances -= 1
+            print("\nWrong Answer!!")
+            print(f"You have {chances} chances left!")
+
+            if chances == 0:
+                print("You Lose!!")
+                playagain()
+                break
+
+print("Thanks for playing!")
